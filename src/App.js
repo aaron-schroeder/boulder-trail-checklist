3138e330611c5bd35296cdf1e572f988ba678ddf
aaron-schroeder <aaschro@gmail.com>
2022-06-06 16:51:45 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component, createRef } from "react"
+import { MapContainer, TileLayer, Marker, Popup, GeoJSON } from 'react-leaflet'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import './App.css';
+import * as trailData from "./data/all_osmp_trails.json";
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.geojsonRef = createRef();
+
+    this.state = {
+      // list: [1, 2, 3],
+      hiked: new Set()
+    };
+
+    this.handleSegSelection = this.handleSegSelection.bind(this);
+    // this.styleGeojson = this.styleGeojson.bind(this);
+  }
+
+  handleSegSelection(id) {
+  
+    const newSet = new Set(this.state.hiked);
+    if (this.state.hiked.has(id)) {
+      newSet.delete(id);
+      this.setState({hiked: newSet})
+    } else {
+      newSet.add(id);
+      this.setState({hiked: newSet})
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          {this.state.hiked}
+        </div>
+        <MapContainer 
+          center={[40.0, -105.25]}
+          zoom={13}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <GeoJSON 
+            key='whatever'
+            data={trailData}
+            attribution='City of Boulder OSMP'
+            ref={this.geojsonRef}
+            // style={this.styleGeojson}
+            // eventHandlers={{
+            //   click: (e) => {
+            //     // console.log(e);
+            //     // console.log(e.target);
+            //     console.log(e.propagatedFrom);
+            //     // e.propagatedFrom.setStyle({color: "#4B1BDE"});
+            //     // setState({'hiked': e.propagatedFrom})
+            //   },
+            //   // mouseover: (e) => {
+            //   //   // console.log(e);
+            //   //   // console.log(e.target);
+            //   //   // e.target.setStyle({color: "#4B1BDE"});
+            //   //   console.log(e.propagatedFrom);
+            //   //   e.propagatedFrom.setStyle({color: "#4B1BDE"});
+            //   // },
+            // }}
+            onEachFeature={(feature, layer) => {
+              layer.on({
+                'mouseover': (e) => {
+                  const segId = feature.properties["GISPROD3.OSMP.TrailsOSMP.SEGMENTID"];
+                  const trailName = feature.properties["GISPROD3.OSMP.TrailsOSMP.TRAILNAME"]
+                  const tooltip = this.state.hiked.has(segId) ? trailName + '<br>Hiked!!' : trailName;
+                  layer.bindTooltip(tooltip);
+                  // layer.openTooltip(e.latlng);
+                  layer.openTooltip();
+                },
+                'mouseout': () => {
+                  layer.unbindTooltip();
+                  layer.closeTooltip();
+                },
+                'click': (e) => {
+                  const segId = feature.properties["GISPROD3.OSMP.TrailsOSMP.SEGMENTID"];
+                  this.handleSegSelection(segId);
+                  if (this.state.hiked.has(segId)) {
+                    layer.setStyle({color: "#4B1BDE"});
+                    // removeSeg(segId);
+                  } else {
+                    // addSeg(segId);
+                    this.geojsonRef.current.resetStyle(e.target);
+                  }
+                },
+              });
+            }}
+          />
+        </MapContainer>
+      </div>
+    );
+  }
+
 }
 
 export default App;
